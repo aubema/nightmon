9146c22be669ff3b8fd72812cc0109d91b789b94
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-11-15 00:57:19 -0500

    changing names

--- observe.bash
+++ observe.bash
@@ -146,8 +146,9 @@ if [ ! -d $basepath/$y/$mo ]
 then /bin/mkdir $basepath/$y/$mo
 fi
 
-# check for sufficient darkness while on park position
+
 take_pictures
+# check for sufficient darkness while on park position
 if [ $tv -lt $max_lum ]
 then "Echo too much light. It is probably daytime."
      move_cams.py 2000 1
@@ -155,11 +156,26 @@ then "Echo too much light. It is probably daytime."
 else move_cams.py 2000 1
 fi
 
-# rename pictures
-mv capture_1.dng $basepath/$y/$m/V-$tv-$basename.dng
-mv capture_1.jpg $basepath/$y/$m/V-$tv-$basename.jpg
-mv capture_2.dng $basepath/$y/$m/R-$tr-$basename.dng
-mv capture_2.jpg $basepath/$y/$m/R-$tr-$basename.jpg
+
+# writing to logfile
 echo $y $mo $d $h $mi $s " V " $tv $basepath/$y/$m/V-$tv-$basename.dng >> $basepath/$y/$m/nightmon.log
 echo $y $mo $d $h $mi $s " R " $tr $basepath/$y/$m/V-$tv-$basename.dng >> $basepath/$y/$m/nightmon.log
 PixelAngles.py >> $basepath/$y/$m/nightmon.log
+echo "=============================="
+# Convert raw images to npy
+
+# Convert
+# rotate and shift sky IMAGES
+python3 TransformImages.py -v capture_1.dng -r capture_2.dng
+mv Vimage.npy $basepath/$y/$m/$basename_V.npy
+mv Rimage.npy $basepath/$y/$m/$basename_R.npy
+# rename pictures
+mv capture_1.dng $basepath/$y/$m/$basename_V.dng
+mv capture_1.jpg $basepath/$y/$m/$basename_V.jpg
+mv capture_2.dng $basepath/$y/$m/$basename_R.dng
+mv capture_2.jpg $basepath/$y/$m/$basename_R.jpg
+mv SolarAngle.npy $basepath/$y/$m/$basename_SolarAngle.npy
+mv MoonAngle.npy $basepath/$y/$m/$basename_MoonAngle.npy
+mv GalacticLattitude.npy $basepath/$y/$m/$basename_GalacticLattitude.npy
+mv AzimuthAngle.npy $basepath/$y/$m/$basename_AzimuthAngle.npy
+mv ZenithAngle.npy $basepath/$y/$m/$basename_ZenithAngle.npy

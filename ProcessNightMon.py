3627d42b0415489cb541f28290650a86dd780dbb
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-11-18 17:27:09 -0500

    added stars recognition

--- ProcessNightMon.py
+++ ProcessNightMon.py
@@ -13,11 +13,17 @@ import numpy as np
 
 import yaml
 from scipy import ndimage
-from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import gaussian_filter
+from scipy.spatial.distance import cdist
 from skyfield.api import E, N, load, wgs84, Star
 from skyfield.framelib import galactic_frame
 from skyfield.data import hipparcos
-
+from astropy.stats import sigma_clipped_stats, SigmaClip
+from astropy.visualization import SqrtStretch
+from astropy.visualization.mpl_normalize import ImageNormalize
+from photutils.aperture import CircularAperture
+from photutils.background import Background2D, MedianBackground
+from photutils.detection import DAOStarFinder
 from toolbox import open_raw, to_grayscale
 
 # find indices of pixels within a radius of a list of alt az coordinates
@@ -53,8 +59,8 @@ with open(home + "/points_list") as pt:
     print("Number of points to extract :", num_pts)
 
 ts = load.timescale()
-# time=ts.utc(2020, 1, 1, 10, 35, 7)
-time = ts.now()
+time=ts.utc(2020, 9, 22, 4, 22, 53)
+#time = ts.now()
 basename = time.utc_strftime('%Y%m%d')
 timestamp = time.utc_strftime('%Y%m%d_%H%M%S')
 # reading command line parameters
@@ -105,15 +111,25 @@ print(f"Sun azimuth: {azis:.4f}")
 Vfile, Rfile = input(sys.argv[1:])
 Vimg = open_raw(Vfile)
 Rimg = open_raw(Rfile)
+# Vimg = 65535.*Vimg
+# Rimg = 65535.*Vimg
 RC = p["R2GrayCoef"]
 GC = p["G2GrayCoef"]
 BC = p["B2GrayCoef"]
 Vgray = to_grayscale(Vimg, [RC, GC, BC], normalize=False)
 Rgray = to_grayscale(Rimg, [RC, GC, BC], normalize=False)
+
+
+
+
+
 ny = np.shape(Vgray)[0]
 nx = np.shape(Vgray)[1]
-
-for band, xzen, yzen, xpol, ypol, imag, outf in (
+Vbkg = np.zeros([ny,nx])
+Rbkg = np.zeros([ny,nx])
+Vstars = np.zeros([ny,nx])
+Rstars = np.zeros([ny,nx])
+for band, xzen, yzen, xpol, ypol, imag, imbkg, imstars, outf in (
     (
         "V",
         p["XzenithV"],
@@ -121,6 +137,8 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
         p["XpolarisV"],
         p["YpolarisV"],
         Vgray,
+        Vbkg,
+        Vstars,
         "VImage.npy",
     ),
     (
@@ -130,6 +148,8 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
         p["XpolarisR"],
         p["YpolarisR"],
         Rgray,
+        Rbkg,
+        Rstars,
         "RImage.npy",
     ),
     ):
@@ -140,6 +160,7 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
     z = p["Zconstant"] + p["Zslope"] * d + p["Zquad"] * d**2
     # calculate azimuth
     azpol = np.arctan2(xpol - xzen, yzen - ypol) * 180 / np.pi
+    print("Rotation angle", azpol)
     az = np.arctan2(-x + xzen, -y + yzen) * 180 / np.pi
     az = az - azpol
     az = np.where(az < 0, az + 360, az)
@@ -151,13 +172,48 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
     az = ndimage.rotate(az, -azpol, reshape=False, mode="nearest")
     z = ndimage.rotate(z, -azpol, reshape=False, mode="nearest")
     imag = ndimage.rotate(imag, -azpol, reshape=False, mode="nearest")
+    # find background
+    sigma_clip = SigmaClip(sigma=3.)
+    bkg_estimator = MedianBackground()
+    # background image
+    bkg = Background2D(imag, (5, 5), filter_size=(3, 3),
+                   sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+    imbkg = bkg.background
+    # image without background
+    imstars = imag - imbkg
+    mean, median, std = sigma_clipped_stats(imstars, sigma=3.0)
+
+
+
+
+    daofind = DAOStarFinder(fwhm=3.0, threshold=5.*std)
+    sources = daofind(imstars)
+    sources = sources[sources['peak'] > 0.5]
+
+
+    # keep stars wit elevation > elmin degrees
+    elmin = 30
+    zemax = 90 - elmin
+    sources = sources[np.sqrt((sources['xcentroid']-nx/2)**2+(sources['ycentroid']-ny/2)**2) < np.sqrt((xzen-xpol)**2+(yzen-ypol)**2)*zemax/(90-p["Latitude"])]
+    for col in sources.colnames:
+        sources[col].info.format = '%.8g'  # for consistent table output
+    print(sources)
+    positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
+    #positions[:, 1] = ny - positions[:, 1]
+    apertures = CircularAperture(positions, r=4.)
+
+
+
+
     # saving sky image
     np.save(outf, imag)
     # mask non sense zenith angles
-    imag[z > 90] = np.nan
-    az[z > 90] = np.nan
+    # imag[z > 90] = np.nan
+    # az[z > 90] = np.nan
+    # imbkg[z > 90] = np.nan
+    # imstars[z > 90] = np.nan
     # TODO : determine clear fraction - here we need to find a way to detect clouds
-    cfrac = 1.
+    cfrac = 0.
     # compute elevation angle
     el = 90 - z
     direction = here_now.from_altaz(alt_degrees=90 - z, az_degrees=az)
@@ -165,6 +221,24 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
     galactic_lat = glat.degrees
     theta_sun = direction.separation_from(sun_position).degrees
     theta_moon = direction.separation_from(moon_position).degrees
+    norm = ImageNormalize(stretch=SqrtStretch())
+
+    # plt.figure()
+    # plt.imshow(imag, cmap="inferno", norm = norm )
+    # plt.colorbar()
+    # plt.title("V image")
+    #
+    # plt.figure()
+    # plt.imshow(imbkg, cmap="inferno", norm = norm)
+    # plt.colorbar()
+    # plt.title("background")
+
+    # plt.figure()
+    # plt.imshow(imstars, cmap="inferno", norm = norm)
+    # apertures.plot(color='white', lw=1.5, alpha=0.5)
+    # plt.colorbar()
+    # plt.title("stars")
+
     # plt.figure()
     # plt.imshow(imag, cmap="rainbow")
     # plt.colorbar()
@@ -198,8 +272,8 @@ for band, xzen, yzen, xpol, ypol, imag, outf in (
 
 
 # creating star map with the SIMBAD database
-limit = 2.  # limitting stars magnitude
-ds = pd.read_csv('simbad_lt_6Vmag_r1.8.csv',header=0,sep=";")
+limit = 3.5  # limitting stars magnitude
+ds = pd.read_csv(home + "/git/nightmon/data/simbad_lt_6Vmag_r1.8.csv",header=0,sep=";")
 stars_selected=ds[ds['MagV'] < limit]
 stars_selected=ds[ds['MagR'] < limit]
 coordsradec = stars_selected["coord1_ICRS,J2000/2000_"]
@@ -229,10 +303,10 @@ for i in range(np.shape(coords)[0]):
     altstar[i]=alt_star
 
 # keep stars above horizon
-azistar[ altstar < 0 ] = np.nan
-magv[ altstar < 0 ] = np.nan
-magr[ altstar < 0 ] = np.nan
-altstar[ altstar < 0 ] = np.nan
+azistar[ altstar < 30 ] = np.nan
+magv[ altstar < 30 ] = np.nan
+magr[ altstar < 30 ] = np.nan
+altstar[ altstar < 30 ] = np.nan
 # remove stars without R magnitude
 azistar[ np.isnan(magr) ] = np.nan
 altstar[ np.isnan(magr) ] = np.nan
@@ -244,46 +318,56 @@ altstar = altstar[ ~np.isnan(altstar) ]
 magv = magv[ ~np.isnan(magv) ]
 magr = magr[ ~np.isnan(magr) ]
 
-plt.plot(azistar,altstar,"or")
-#plt.show()
 
-index , pindex = find_close_indices(az, el, azistar , altstar, 0.5)
+
+
+index , pindex = find_close_indices(az, el, azistar , altstar, 1)
 pshape=int(np.shape(pindex)[0])
 pshape=int(pshape/2)
 pindex = pindex.reshape(pshape,2)
+print(pindex)
+# match database stars with detected stars
+
+def closest_node(node, nodes):
+    print(cdist([node], nodes).argmin(), nodes[cdist([node], nodes).argmin()],node)
+    dist = np.sqrt((nodes[cdist([node], nodes).argmin()][0]-node[0])**2+(nodes[cdist([node], nodes).argmin()][1]-node[1])**2)
+    print("dist=",dist)
+    return nodes[cdist([node], nodes).argmin()]
+
+
 falseskyV = np.zeros([ny,nx],dtype=float)
 falseskyR = np.zeros([ny,nx],dtype=float)
 for ns in range(pshape):
     print(pindex[ns,0],pindex[ns,1])
+    a_star = (pindex[ns,0],pindex[ns,1])
+    closest_node(a_star, positions)
+
+
     falseskyV[pindex[ns,0],pindex[ns,1]]=magv[ns]
     falseskyR[pindex[ns,0],pindex[ns,1]]=magr[ns]
+
+plt.figure()
+plt.plot(positions[:,0],positions[:,1],"or")
+plt.plot(pindex[:,1],pindex[:,0],"ob")
+
 #falseskyV[falseskyV == 0] = np.nan
 #falseskyR[falseskyR == 0] = np.nan
-falseskyV[el < 0] = np.nan
-falseskyR[el < 0] = np.nan
-plt.figure()
-plt.imshow(falseskyV, cmap="inferno")
-plt.colorbar()
-plt.title("Stars in V band")
-
-plt.figure()
-plt.imshow(falseskyR, cmap="inferno")
-plt.colorbar()
-plt.title("Stars in R band")
-
-
-
-#plt.show()
-
-
-
-
-
+# falseskyV[el < 0] = np.nan
+# falseskyR[el < 0] = np.nan
+# plt.figure()
+# plt.imshow(falseskyV, cmap="inferno", norm=norm)
+# plt.colorbar()
+# plt.title("Database stars in V band")
+#
+# plt.figure()
+# plt.imshow(falseskyR, cmap="inferno", norm=norm)
+# plt.colorbar()
+# plt.title("Database stars in R band")
 
 # Extract and write data
 sberr = 0
 # mflag = moon flag 1 moon in the sky 0 no moon
-# cfrac = clear fraction = 1 - cloudfraction
+# cfrac = cloud fraction
 pazi = np.zeros([1], dtype=float)
 pele = np.zeros([1], dtype=float)
 prad = np.zeros([1], dtype=float)
@@ -318,15 +402,15 @@ for no in range(num_pts):
         sb=zerop * 10 ** (-0.4*rad)
         sberr = 0.
         outputline = timestamp + " " + str(posx) + " " + str(posy) + " " + str("{:.6f}".format(pazi[0])) + " " + str("{:.6f}".format(pele[0])) + " " + str("{:.6f}".format(sb)) + " " + str("{:.6f}".format(sberr)) + " " + str(mflag) + " " + str("{:.6f}".format(cfrac)) + " " + str("{:.6f}".format(np.mean(theta_sun[pindex[0],pindex[1]]))) + " " + str("{:.6f}".format(np.mean(theta_moon[pindex[0],pindex[1]]))) + " " + str("{:.6f}".format(np.mean(galactic_lat[pindex[0],pindex[1]]))) + " " +  str("{:.6f}".format(prad[0])) + "\n"
-        print(outputline)
+        print(band," : ",outputline)
         o.write(outputline)
         o.close()
 
 
-plt.figure()
-plt.imshow(np.round(theta_sun), cmap="rainbow")
-plt.colorbar()
-plt.title("Solar angle")
+# plt.figure()
+# plt.imshow(np.round(theta_sun), cmap="rainbow")
+# plt.colorbar()
+# plt.title("Solar angle")
 
 # plt.figure()
 # plt.imshow(np.round(theta_moon), cmap="rainbow")

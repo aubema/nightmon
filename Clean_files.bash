36f2135387777280139d852885331ddbf7a795f4
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-12-02 11:52:59 -0500

    adding backup

--- Clean_files.bash
+++ Clean_files.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 # clean sd card
-path_to_clean=`date '+%Y/%m' --date '1 month ago'``
-#remove the oldest images
-rm  ls -l --sort=time /var/www/html/data/$path_to_clean/*_R_*.dng | sed -n 2p  | awk '{print $NF}'
-rm  ls -l --sort=time /var/www/html/data/$path_to_clean/*_V_*.dng | sed -n 2p  | awk '{print $NF}'
-rm  ls -l --sort=time /var/www/html/data/$path_to_clean/*_V_*.jpg | sed -n 2p  | awk '{print $NF}'
-rm  ls -l --sort=time /var/www/html/data/$path_to_clean/*_V_*.jpg | sed -n 2p  | awk '{print $NF}'
+path_to_compress=`date '+%Y/%m' --date '1 month ago'``
+path_to_delete=`date '+%Y/%m' --date '6 month ago'``
+#compress images of the previous month
+gzip /var/www/html/data/$path_to_compress/*.dng
+#remove the old images
+rm  -f /var/www/html/data/$path_to_delete/*.dng.gz

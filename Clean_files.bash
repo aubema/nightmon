5f5367c24c93c242a95183ec256f8c096aedae5c
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-12-06 18:21:52 -0500

    cleaning script

--- Clean_files.bash
+++ Clean_files.bash
@@ -1,8 +1,21 @@
 #!/bin/bash
+# this script should run once a day and scheduled with crontab at 0 12 * * *
 # clean sd card
-path_to_compress=`date '+%Y/%m' --date '1 month ago'``
-path_to_delete=`date '+%Y/%m' --date '6 month ago'``
+path_to_compress=`date '+%Y/%m' --date '2 week ago'``
+day_to_compress=`date '+%d' --date '2 week ago'``
+path_to_delete=`date '+%Y/%m' --date '4 week ago'``
+day_to_delete=`date '+%d' --date '4 week ago'``
 #compress images of the previous month
-gzip /var/www/html/data/$path_to_compress/*.dng
+gzip /var/www/html/data/$path_to_compress/*$day_to_compress_*.dng
 #remove the old images
-rm  -f /var/www/html/data/$path_to_delete/*.dng.gz
+rm  -f /var/www/html/data/$path_to_delete/*$day_to_delete_*.dng.gz
+rm  -f /var/www/html/data/$path_to_delete/*$day_to_delete_*.jpg
+path_to_compress=`date '+%Y/%m' --date '8 week ago'``
+day_to_compress=`date '+%d' --date '8 week ago'``
+path_to_delete=`date '+%Y/%m' --date '16 week ago'``
+day_to_delete=`date '+%d' --date '16 week ago'``
+#compress images
+gzip /home/sand/data/$path_to_compress/*$day_to_compress_*.dng
+#remove the old images
+rm  -f /home/sand/data/$path_to_delete/*$day_to_delete_*.dng.gz
+rm  -f /home/sand/data/$path_to_delete/*$day_to_delete_*.jpg

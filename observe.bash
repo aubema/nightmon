25370c6e6c1de94948b9f2aa68e41808e5a53d4b
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2022-12-13 14:02:01 -0500

    bad path

--- observe.bash
+++ observe.bash
@@ -191,8 +191,11 @@ do 	if [ $n -eq 0 ]
 
 		# backup output files
 		cp -f $basepath/$y/$mo/nightmon.log $backpath/$y/$mo/
-		cat "calibrated_"$b"_"$baseday"_sky.csv" | grep -v "Loc_Name" | grep -v "(pixel)"  >> $basepath/$y/$mo/"calibrated_"$b"_"$baseday"_sky.csv"
-		cp -f $basepath/$y/$mo/"calibrated_"$b"_"$baseday"_sky.csv" $backpath/$y/$mo/
+		if [ -f $basepath/$y/$mo/"calibrated_"$baseday"_sky.csv" ]
+		then cat "calibrated_"$b"_"$baseday"_sky.csv" | grep -v "Loc_Name" | grep -v "(pixel)"  >> $basepath/$y/$mo/"calibrated_"$baseday"_sky.csv"
+		else cat "calibrated_"$b"_"$baseday"_sky.csv" | grep "Loc_Name" | grep "(pixel)"  > $basepath/$y/$mo/"calibrated_"$baseday"_sky.csv"
+		fi
+		cp -f $basepath/$y/$mo/"calibrated_"$baseday"_sky.csv" $backpath/$y/$mo/
 
 		# clean directory
 		rm ${basename[$n]}"_"${cams[$n]}"_"$t"_"$gain".dng"

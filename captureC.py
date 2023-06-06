c7fa55e14d418718737a75b31b0f4a4a739075f7
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2023-06-06 18:13:49 -0400

    immediate

--- captureC.py
+++ captureC.py
@@ -50,7 +50,7 @@ def capture(cam, itime, gain):
         + str(gain)
         + " --shutter "
         + str(itime)
-        + " --denoise off --rawfull --raw --awbgains 1,1 --immediate --nopreview -o /home/sand/capture_1.jpg"
+        + " --denoise off --rawfull --raw --awbgains 1,1 --immediate --nopreview -o /home/sand/capture_3.jpg"
     )
     os.system(cmd)
 
